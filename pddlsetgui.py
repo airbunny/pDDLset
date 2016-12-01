e7b93a689296cb86fdd8cc53b039e91062a00917
airbunny <airbunny@126.com>
2016-12-01 22:24:13 +0800

    some work whit thread.and some bug should be check out and fix it

--- pddlsetgui.py
+++ pddlsetgui.py
@@ -42,304 +42,297 @@ elif pDDLType =="AP01":
 #default Network ID
 NetWorkID = "11101L249"
 TelnetIsRun = False
-
+Counter = 0
 
 #######################################
 #           Telnet process
 #######################################
 def TelnetProcess():
+    tn = telnetlib.Telnet()
+    tn.set_debuglevel(2)
 
-    while True:
-        if TelnetIsRun == True:
-            tn = telnetlib.Telnet()
-            tn.set_debuglevel(2)
+    print ("Connecting...");
 
-            print ("Connecting...");
+    #
+    #  Connect telnet
+    #
+    try:
+        tn.open(HOST)
+        print ("Connected!");
+        res = True
+    except:
+        print ("Can not link host");
+        tn.close();
+        app.enableall();
+        return False
 
-            #
-            #  Connect telnet
-            #
-            try:
-                tn.open(HOST)
-                print ("Connected!");
-                res = True
-            except:
-                print ("Can not link host");
-                tn.close();
-                app.enableall();
-                return False
+    print("start reading...");
+    time.sleep(5);
 
-            print("start reading...");
-            time.sleep(5);
+    #
+    #   input username
+    #
+    try:
+        print(tn.read_until(b"login"));
+        time.sleep(2);
+        tn.write(b"admin"+b"\n");
+        print("username input passed");
+    except:
+        print("can not input username!");
+        tn.close();
+        return False
 
-            #
-            #   input username
-            #
-            try:
-                print(tn.read_until(b"login"));
-                time.sleep(2);
-                tn.write(b"admin"+b"\n");
-                print("username input passed");
-            except:
-                print("can not input username!");
-                tn.close();
-                return False
+    #
+    #   input password
+    #
+    time.sleep(2);
+    print("password inputing...");
+    try:
+        print(tn.read_some());
+        time.sleep(2);
+        tn.write(PASSWORD+b"\n");
+        print("password input passed");
+    except:
+        print("can not input password");
+        tn.close();
+        return False
 
-            #
-            #   input password
-            #
-            time.sleep(2);
-            print("password inputing...");
-            try:
-                print(tn.read_some());
-                time.sleep(2);
-                tn.write(PASSWORD+b"\n");
-                print("password input passed");
-            except:
-                print("can not input password");
-                tn.close();
-                return False
+    #
+    # comfirm login
+    #
+    time.sleep(5);
+    try:
+        command = tn.read_until(b"evice>");
+        print(command);
+        drawline();
+    except:
+        print("poland can not into space");
+        return False
 
-            #
-            # comfirm login
-            #
-            time.sleep(5);
-            try:
-                command = tn.read_until(b"evice>");
-                print(command);
-                drawline();
-            except:
-                print("poland can not into space");
-                return False
+    #
+    #  login sucsses and input network id
+    #
+    print("  ");
+    print("  ");
+    drawline();
+    print("login sucsses!!!");
+    print("  ");
+    print("  ");
+    print("  ");
+    print("****************************************************");
+    print("Now writing command.....");
+    print("   ");
 
-            #
-            #  login sucsses and input network id
-            #
-            print("  ");
-            print("  ");
-            drawline();
-            print("login sucsses!!!");
-            print("  ");
-            print("  ");
-            print("  ");
-            print("****************************************************");
-            print("Now writing command.....");
-            print("   ");
+    #
+    #   writing setup data
+    #
 
-            #
-            #   writing setup data
-            #
+    #write wan bright
+    tn.write(b"at+mnwan"+b"\n");
+    time.sleep(2);
+    try:
+        command = tn.read_until(b"OK");
+        print("wan bright set sucsses!");
+        drawline();
+    except:
+        print("Error 101");
+        return False
 
-            #write wan bright
-            tn.write(b"at+mnwan"+b"\n");
-            time.sleep(2);
-            try:
-                command = tn.read_until(b"OK");
-                print("wan bright set sucsses!");
-                drawline();
-            except:
-                print("Error 101");
-                return False
+    #write wan set
+    tn.write(b"at+mnwan=1"+b"\n");
+    time.sleep(2);
+    try:
+        command = tn.read_until(b"OK");
+        print("wan set sucsses!");
+        drawline();
+    except:
+        print("Error 102");
+        return False
 
-            #write wan set
-            tn.write(b"at+mnwan=1"+b"\n");
-            time.sleep(2);
-            try:
-                command = tn.read_until(b"OK");
-                print("wan set sucsses!");
-                drawline();
-            except:
-                print("Error 102");
-                return False
+    #write lan set
+    tn.write(b"at+mcps2=1"+b"\n");
+    time.sleep(2);
+    try:
+        command = tn.read_until(b"OK");
+        print("lan set sucsses!");
+        drawline();
+    except:
+        print("Error 103");
+        return False
 
-            #write lan set
-            tn.write(b"at+mcps2=1"+b"\n");
-            time.sleep(2);
-            try:
-                command = tn.read_until(b"OK");
-                print("lan set sucsses!");
-                drawline();
-            except:
-                print("Error 103");
-                return False
+    #write some set
+    tn.write(b"at+mcbr2=13"+b"\n");
+    time.sleep(2);
+    try:
+        command = tn.read_until(b"OK");
+        print("some set sucsses!");
+        drawline();
+    except:
+        print("Error 105");
+        return False
 
-            #write some set
-            tn.write(b"at+mcbr2=13"+b"\n");
-            time.sleep(2);
-            try:
-                command = tn.read_until(b"OK");
-                print("some set sucsses!");
-                drawline();
-            except:
-                print("Error 105");
-                thread.exit()
-                return False
+    #write whatever set
+    tn.write(b"at+mcdf2=0"+b"\n");
+    time.sleep(2);
+    try:
+        command = tn.read_until(b"OK");
+        print("whatever set sucsses!");
+        drawline();
+    except:
+        print("Error 106");
+        return False
 
-            #write whatever set
-            tn.write(b"at+mcdf2=0"+b"\n");
-            time.sleep(2);
-            try:
-                command = tn.read_until(b"OK");
-                print("whatever set sucsses!");
-                drawline();
-            except:
-                print("Error 106");
-                thread.exit()
-                return False
+    #write somehow set
+    tn.write(b"at+mcdm2=0"+b"\n");
+    time.sleep(2);
+    try:
+        command = tn.read_until(b"OK");
+        print("somehow set sucsses!");
+        drawline();
+    except:
+        print("Error 109");
+        return False
 
-            #write somehow set
-            tn.write(b"at+mcdm2=0"+b"\n");
-            time.sleep(2);
-            try:
-                command = tn.read_until(b"OK");
-                print("somehow set sucsses!");
-                drawline();
-            except:
-                print("Error 109");
-                thread.exit()
-                return False
+    #write #01 set
+    tn.write(b"at+mcct2=1024"+b"\n");
+    time.sleep(2);
+    try:
+        command = tn.read_until(b"OK");
+        print("#01 set sucsses!");
+        drawline();
+    except:
+        print("Error 201");
+        return False
 
-            #write #01 set
-            tn.write(b"at+mcct2=1024"+b"\n");
-            time.sleep(2);
-            try:
-                command = tn.read_until(b"OK");
-                print("#01 set sucsses!");
-                drawline();
-            except:
-                print("Error 201");
-                thread.exit()
-                return False
+    #write #02 set
+    tn.write(b"at+mcipm2=2"+b"\n");
+    time.sleep(2);
+    try:
+        command = tn.read_until(b"OK");
+        print("#02 set sucsses!");
+        drawline();
+    except:
+        print("Error 204");
+        return False
 
-            #write #02 set
-            tn.write(b"at+mcipm2=2"+b"\n");
-            time.sleep(2);
-            try:
-                command = tn.read_until(b"OK");
-                print("#02 set sucsses!");
-                drawline();
-            except:
-                print("Error 204");
-                thread.exit()
-                return False
+    #write seria target set
+    if pDDLTarg == "Air":
+        tn.write(b"at+mctcs2=192.168.1.11,20002,60,0,10,20002,300"+b"\n");
+    elif pDDLTarg == "Ground":
+        tn.write(b"at+mctcs2=192.168.1.12,20002,60,0,10,20002,300"+b"\n");
+    time.sleep(2);
+    try:
+        command = tn.read_until(b"OK");
+        print("seria target set sucsses!");
+        drawline();
+    except:
+        print("Error 209");
+        return False
 
-            #write seria target set
-            if pDDLTarg == "Air":
-                tn.write(b"at+mctcs2=192.168.1.11,20002,60,0,10,20002,300"+b"\n");
-            elif pDDLTarg == "Ground":
-                tn.write(b"at+mctcs2=192.168.1.12,20002,60,0,10,20002,300"+b"\n");
-            time.sleep(2);
-            try:
-                command = tn.read_until(b"OK");
-                print("seria target set sucsses!");
-                drawline();
-            except:
-                print("Error 209");
-                thread.exit()
-                return False
+    #write seria band set
+    tn.write(b"at+mwband=0"+b"\n");
+    time.sleep(2);
+    try:
+        command = tn.read_until(b"OK");
+        print("seria band set sucsses!");
+        drawline();
+    except:
+        print("Error 211");
+        return False
 
-            #write seria band set
-            tn.write(b"at+mwband=0"+b"\n");
-            time.sleep(2);
-            try:
-                command = tn.read_until(b"OK");
-                print("seria band set sucsses!");
-                drawline();
-            except:
-                print("Error 211");
-                thread.exit()
-                return False
+    #write RF frequse set
+    tn.write(b"at+mwfreq=76"+b"\n");
+    time.sleep(2);
+    try:
+        command = tn.read_until(b"OK");
+        print("RF freqnse set sucsses!");
+        drawline();
+    except:
+        print("Error 233");
+        return False
 
-            #write RF frequse set
-            tn.write(b"at+mwfreq=76"+b"\n");
-            time.sleep(2);
-            try:
-                command = tn.read_until(b"OK");
-                print("RF freqnse set sucsses!");
-                drawline();
-            except:
-                print("Error 233");
-                thread.exit()
-                return False
+    #write RF mode set
+    tn.write(b"at+mwvmode=0"+b"\n");
+    time.sleep(2);
+    try:
+        command = tn.read_until(b"OK");
+        print("RF mode set sucsses!");
+        drawline();
+    except:
+        print("Error sm");
+        return False
 
-            #write RF mode set
-            tn.write(b"at+mwvmode=0"+b"\n");
-            time.sleep(2);
-            try:
-                command = tn.read_until(b"OK");
-                print("RF mode set sucsses!");
-                drawline();
-            except:
-                print("Error sm");
-                thread.exit()
-                return False
-
-            #write lan basic set
-            if pDDLTarg == "Ground":
-                tn.write(b"at+mnlan=lan,EDIT,0,192.168.1.11,255.255.255.0"+b"\n");
-            elif pDDLTarg == "Air":
-                tn.write(b"at+mnlan=lan,EDIT,0,192.168.1.12,255.255.255.0"+b"\n");
-            time.sleep(2);
-            try:
-                command = tn.read_until(b"OK");
-                print("lan basic set sucsses!");
-                drawline();
-            except:
-                print("Error lan");
-                thread.exit()
-                return False
-            
-            #write network id.....
-            tn.write(b"at+mwnetworkid="+bytes(NetWorkID,encoding ="utf-8")+b"\n");
-            time.sleep(2);
-            try:
-                command = tn.read_until(b"OK");
-                print("network id set sucsses!");
-                drawline();
-            except:
-                print("Error 401");
-                thread.exit()
-                return False
+    #write lan basic set
+    if pDDLTarg == "Ground":
+        tn.write(b"at+mnlan=lan,EDIT,0,192.168.1.11,255.255.255.0"+b"\n");
+    elif pDDLTarg == "Air":
+        tn.write(b"at+mnlan=lan,EDIT,0,192.168.1.12,255.255.255.0"+b"\n");
+    time.sleep(2);
+    try:
+        command = tn.read_until(b"OK");
+        print("lan basic set sucsses!");
+        drawline();
+    except:
+        print("Error lan");
+        return False
+        
+    #write network id.....
+    tn.write(b"at+mwnetworkid="+bytes(NetWorkID,encoding ="utf-8")+b"\n");
+    time.sleep(2);
+    try:
+        command = tn.read_until(b"OK");
+        print("network id set sucsses!");
+        drawline();
+    except:
+        print("Error 401");
+        return False
 
 
-            #
-            # final move
-            #
+    #
+    # final move
+    #
 
-            #Enable all set
-            tn.write(b"at&W"+b"\n");
-            drawline();
-            print("now solid all seting...");
-            print(".");
-            print(".");
-            print(".");
-            print(".");
-            print(".");
-            print(".");
-            time.sleep(8);
-            try:
-                command = tn.read_until(b"recently");
-                print("All set had been writen in pDDL!");
-                drawline();
-            except:
-                print("Failed to solid seting!");
+    #Enable all set
+    tn.write(b"at&W"+b"\n");
+    drawline();
+    print("now solid all seting...");
+    print(".");
+    print(".");
+    print(".");
+    print(".");
+    print(".");
+    print(".");
+    time.sleep(8);
+    try:
+        command = tn.read_until(b"recently");
+        print("All set had been writen in pDDL!");
+        drawline();
+    except:
+        print("Failed to solid seting!");
 
-                return False
+        return False
 
-            #exit command interface
-            time.sleep(2)
-            tn.write(b"ata");
-            #close telnet
-            time.sleep(6)
-            tn.close()
+    #exit command interface
+    time.sleep(2)
+    tn.write(b"ata");
+    #close telnet
+    time.sleep(6)
+    tn.close()
 
-            print("pDDL setup sucsses!");
-            print("****************************************************");
-            print("   ");
+    print("pDDL setup sucsses!");
+    print("****************************************************");
+    print("   ");
     
-    print("Thread exit...")
-
     return True
+#########################################
+#     thread function
+#########################################
+def threadfun():
+    while True: #ThreadIsRun.isSet():
+        time.sleep(1)
+        Counter +=  1
+        print("%d:2211334"%Counter)
+    return
 
 #########################################
 #     windows class
@@ -367,11 +360,19 @@ class Application(tk.Frame):
         self.grset["text"] = "set pddl ground side"
         self.grset["command"] = self.telnetground
         self.grset.grid(row=2,column=1)
+        self.showset = tk.Button(self)
+        self.showset["text"] = "show set"
+        self.showset["command"] = self.setmessagebox
+        self.showset.grid(row=5,column=1)
         
         self.quit = tk.Button(self, text="QUIT",
-                              command=root.destroy)
+                              command=self.quitbox)
         self.quit.grid(row=3,column=1)
     
+    def quitbox(self):
+        #ThreadIsRun = False
+        ThreadIsRun.clear()
+        root.destroy()
     #set pddl as airside
     def telnetair(self):
         #when telnet client is working disable all button to keeping safe 
@@ -381,7 +382,6 @@ class Application(tk.Frame):
         #and just start working...
         TelnetIsRun = True
         #enable button after working
-        #self.enableall()
 
     #set pddl as groundside
     def telnetground(self):
@@ -390,9 +390,10 @@ class Application(tk.Frame):
         #set paraments to air side
         pDDLTarg = "Ground"
         #and just start working...
-        TelnetIsRun = True
+        TelnetProcess()
+        #TelnetIsRun = True
+        print("test")
         #enable button after working
-        #self.enableall()
 
     #enable all button after telnet work
     def enableall(self):
@@ -403,15 +404,23 @@ class Application(tk.Frame):
         self.airset["state"] = "disable"
         self.grset["state"] = "disable"
 
-    #def plussnID(self):
-        
+    def setmessagebox(self):
+        messagebox.showinfo("猞猁饲养指南","Password:%s  IP:%s"%(PASSWORD,HOST)) 
+        messagebox.showinfo("猞猁饲养指南","what?%d"%thread.isAlive())
+        TelnetRun.clear()   
 
 
 ###########################################
 #       main process
 ###########################################
 
-thread = threading.Thread(target=TelnetProcess)
+# 条件变量
+#con = threading.Condition()
+ThreadIsRun = threading.Event()
+TelnetRun = threading.Event()
+ThreadIsRun.set()
+TelnetRun.set()
+thread = threading.Thread(target=threadfun)
 thread.start()
 
 root = tk.Tk()
